cb07f74712694a1bc340540c5ecc6033ac965208
johann681 <adekolaolaonipekun@gmail.com>
2025-01-18 02:33:28 +0100

    Updates CSS styles and adds functionality to read more text
    Improves CSS styles by adding font family and font size
    Adds functionality to show/hide additional text when clicking "Read more" button
    
    Relates to #123

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-let text = document.getElementById('text');
-
-window.addEventListener('scroll', () =>{
-    let value = window.scrollY;
-    text.style.marginTop = value * 2.5 + 'px';
-})
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
